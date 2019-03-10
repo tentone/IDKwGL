a14e2a78be821f458b2b32c0c63058e077f7409b
Tentone <tentone@outlook.com>
2019-03-10 12:37:40 +0000

    Clean up particl

--- core/objects/particle/Particle.js
+++ core/objects/particle/Particle.js
@@ -7,13 +7,13 @@
  */
 function Particle(object, position, speed, scale, time)
 {
-	this.position = new Vector3(position.x, position.y, position.z);
-	this.scale = new Vector3(scale, scale, scale);
+	this.object = object;
+	this.object.position.set(position.x, position.y, position.z);
+	this.object.scale.set(scale, scale, scale);
+
 	this.speed = new Vector3(speed.x, speed.y, speed.z);
 
 	this.time = time;
-
-	this.object = object;
 }
 
 Particle.prototype.constructor = Particle;
@@ -23,7 +23,7 @@ Particle.prototype.constructor = Particle;
  */
 Particle.prototype.update = function()
 {
-	this.position.add(this.speed);
+	this.object.position.add(this.speed);
 	this.time--;
 };
 
@@ -32,8 +32,6 @@ Particle.prototype.update = function()
  */
 Particle.prototype.render = function(renderer, camera, scene)
 {
-	this.object.position.set(this.position.x, this.position.y, this.position.z);
-	this.object.scale.set(this.scale.x, this.scale.y, this.scale.z);
-	this.object.updateMatrix();
+	this.object.updateMatrix();	
 	this.object.render(renderer, camera, scene);
 };
--- core/objects/particle/ParticleEmitter.js
+++ core/objects/particle/ParticleEmitter.js
@@ -5,25 +5,53 @@
  *
  * Particle have a speed, scale and time limit and wrap a drawable object.
  */
-function ParticleEmitter(model, position, speed, speedVar, scale, scaleVar, time, timeVar, particleCount)
+function ParticleEmitter(object, position, speed, speedVar, scale, scaleVar, time, timeVar, count)
 {
+	/** 
+	 * Base object of the emitter, the object is cloned for each particle.
+	 */
+	this.object = object;
+
+	/**
+	 * Particles in the emitter, to be updated and rendered.
+	 */
 	this.particles = [];
-	this.particleCount = particleCount;
+
+	/**
+	 * Number of particles in the emitter.
+	 */
+	this.count = count;
+	
+	/** 
+	 * Position of the particles.
+	 */
 	this.position = position;
+	this.positionVar = new Vector3(0, 0, 0);
+
+	/**
+	 * Base speed of the particle moving.
+	 */
 	this.speed = speed;
 	this.speedVar = speedVar;
+	
+	/**
+	 * Base scale of the particle.
+	 */
 	this.scale = scale;
 	this.scaleVar = scaleVar;
+	
+	/**
+	 * Lifetime of a individual particle.
+	 */
 	this.time = time;
 	this.timeVar = timeVar;
-	this.model = model;
-
-	var speed = new Vector3();
-
-	for(var i = 0; i < this.particleCount; i++)
+	
+	for(var i = 0; i < this.count; i++)
 	{
-		speed.set(this.speed.x + this.speedVar.x*MathUtils.randomMod(), this.speed.y + this.speedVar.y*MathUtils.randomMod(), this.speed.z + this.speedVar.z*MathUtils.randomMod());
-		this.particles.push(new Particle(this.model, this.position, speed, this.scale + this.scaleVar*MathUtils.randomMod(), this.time + this.timeVar*MathUtils.randomMod()));
+		var speed = new Vector3();
+		speed.set(this.speed.x + this.speedVar.x * MathUtils.randomMod(), this.speed.y + this.speedVar.y * MathUtils.randomMod(), this.speed.z + this.speedVar.z * MathUtils.randomMod());
+
+		this.particles.push(new Particle(this.object, this.position, speed, this.scale + this.scaleVar * MathUtils.randomMod(), this.time + this.timeVar * MathUtils.randomMod()));
 	}
 }
 
@@ -41,11 +69,13 @@ ParticleEmitter.prototype.update = function()
 		//Reset particle
 		if(this.particles[i].time < 0)
 		{
-			this.particles[i].position.copy(this.position);
+			this.particles[i].object.position.copy(this.position);
+			
 			var scale = this.scale + this.scaleVar * MathUtils.randomMod();
-			this.particles[i].scale.set(scale, scale, scale);
-			this.particles[i].time = this.time + this.timeVar*MathUtils.randomMod();
-			this.particles[i].speed.set(this.speed.x + this.speedVar.x*MathUtils.randomMod(), this.speed.y + this.speedVar.y*MathUtils.randomMod(), this.speed.z + this.speedVar.z*MathUtils.randomMod());
+			this.particles[i].object.scale.set(scale, scale, scale);
+
+			this.particles[i].time = this.time + this.timeVar * MathUtils.randomMod();
+			this.particles[i].speed.set(this.speed.x + this.speedVar.x * MathUtils.randomMod(), this.speed.y + this.speedVar.y * MathUtils.randomMod(), this.speed.z + this.speedVar.z * MathUtils.randomMod());
 		}
 	}
 };
