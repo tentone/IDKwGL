66e734e0bc758d935c584967f37620b1cf07c2e6
Tentone <tentone@outlook.com>
2019-04-23 15:59:18 +0100

    Derp

--- core/postprocessing/Pass.js
+++ core/postprocessing/Pass.js
@@ -4,3 +4,8 @@ function Pass()
 {
 	//TODO <ADD CODE HERE>
 }
+
+Pass.prototype.render = function(scene, camera, inputRenderTarget, outputRenderTarget)
+{
+	//TODO <ADD CODE HERE>
+};
--- core/postprocessing/Postprocessing.js
+++ core/postprocessing/Postprocessing.js
@@ -1,8 +1,26 @@
 "use strict";
 
+/**
+ * Class used to craete and manage a post processing pipeline.
+ *
+ * A post processing pipeline is composed of passes executed in order.
+ */
 function Postprocessing()
 {
+	/**
+	 * List of render passes.
+	 */
 	this.passes = [];
+
+	/**
+	 * Input buffer passed to the render pass.
+	 */
+	this.inputRenderTarget = null;
+
+	/**
+	 * Output render target, where for the render pass to write its final result.
+	 */
+	this.outputRenderTarget = null;
 }
 
 Postprocessing.prototype.render = function(scene, camera, renderTarget)
